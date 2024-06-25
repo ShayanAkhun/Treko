850881fa395b70618c0521502123451a69c4ac4f
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-06-26 01:26:32 +0500

    changes

--- src/stacks/MenuStack.tsx
+++ src/stacks/MenuStack.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Menu from '../screens/Menu/Menu';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export  const MenuStack = () => {
   return (
-    <View>
-      <Text>Menu screen</Text>
-    </View>
+    <Stack.Navigator initialRouteName="Find">
+    <Stack.Screen
+      name="LocationScreen"
+      component={Menu}
+      options={{
+        headerShown: false,
+        title: 'Find',
+      }}
+    />
+  </Stack.Navigator>
   );
 }
 
